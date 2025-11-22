0d837b62cc3651ee6ac9893febe80b4aec7fc42a
riyanwardhana <riyanwardhana2@gmail.com>
2025-11-23 01:08:14 +0700

    Fix: Navbar optimization

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,61 +1,85 @@
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
-import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/Navbar.css";
 
 function Navbar() {
   const { authUser, logout } = useContext(AuthContext);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   const handleLogout = () => {
     logout();
+    closeMobileMenu();
   };
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        {/* LOGO */}
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           SiMathUNIMED
         </Link>
-        <ul className="navbar-menu">
+
+        {/* IKON HAMBURGER (Hanya muncul di Mobile) */}
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+
+        {/* MENU LINKS */}
+        <ul className={click ? "navbar-menu active" : "navbar-menu"}>
           <li className="navbar-item">
-            <Link to="/" className="navbar-link">
+            <Link to="/" className="navbar-link" onClick={closeMobileMenu}>
               Beranda
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/profil" className="navbar-link">
+            <Link
+              to="/profil"
+              className="navbar-link"
+              onClick={closeMobileMenu}
+            >
               Profil Jurusan
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/akademik" className="navbar-link">
+            <Link
+              to="/akademik"
+              className="navbar-link"
+              onClick={closeMobileMenu}
+            >
               Akademik
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/dosen" className="navbar-link">
+            <Link to="/dosen" className="navbar-link" onClick={closeMobileMenu}>
               Dosen
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/berita" className="navbar-link">
+            <Link
+              to="/berita"
+              className="navbar-link"
+              onClick={closeMobileMenu}
+            >
               Berita
             </Link>
           </li>
+
+          {/* AUTH LINKS */}
           {authUser ? (
             <>
               <li className="navbar-item">
-                <Link to="/admin/dashboard" className="navbar-link">
+                <Link
+                  to="/admin/dashboard"
+                  className="navbar-link"
+                  onClick={closeMobileMenu}
+                >
                   Dashboard
                 </Link>
               </li>
-              {/* <li className="navbar-item">
-                <span
-                  className="navbar-link"
-                  style={{ color: "#f7f7f7", cursor: "default" }}
-                >
-                  Hi, {authUser.nama_lengkap}
-                </span>
-              </li> */}
               <li className="navbar-item">
                 <button onClick={handleLogout} className="navbar-link-button">
                   Logout
@@ -64,7 +88,11 @@ function Navbar() {
             </>
           ) : (
             <li className="navbar-item">
-              <Link to="/login" className="navbar-link">
+              <Link
+                to="/login"
+                className="navbar-link"
+                onClick={closeMobileMenu}
+              >
                 Login
               </Link>
             </li>
