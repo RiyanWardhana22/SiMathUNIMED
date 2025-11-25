76ea34afa1c18a85e313280637b8aab99d69b62f
riyanwardhana <riyanwardhana2@gmail.com>
2025-11-25 16:16:18 +0700

    Add: Dropdown at Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaCaretDown } from "react-icons/fa";
 import "../styles/Navbar.css";
 
 function Navbar() {
   const { authUser, logout } = useContext(AuthContext);
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
   const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu = () => {
+    setClick(false);
+    setDropdown(false);
+  };
+
+  const toggleDropdown = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(!dropdown);
+    }
+  };
 
   const handleLogout = () => {
     logout();
@@ -18,7 +28,6 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* LOGO */}
         <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           SiMathUNIMED
         </Link>
@@ -27,13 +36,13 @@ function Navbar() {
           {click ? <FaTimes /> : <FaBars />}
         </div>
 
-        {/* MENU LINKS */}
         <ul className={click ? "navbar-menu active" : "navbar-menu"}>
           <li className="navbar-item">
             <Link to="/" className="navbar-link" onClick={closeMobileMenu}>
               Beranda
             </Link>
           </li>
+
           <li className="navbar-item">
             <Link
               to="/profil"
@@ -43,20 +52,13 @@ function Navbar() {
               Profil Jurusan
             </Link>
           </li>
-          <li className="navbar-item">
-            <Link
-              to="/akademik"
-              className="navbar-link"
-              onClick={closeMobileMenu}
-            >
-              Akademik
-            </Link>
-          </li>
+
           <li className="navbar-item">
             <Link to="/dosen" className="navbar-link" onClick={closeMobileMenu}>
               Dosen
             </Link>
           </li>
+
           <li className="navbar-item">
             <Link
               to="/berita"
@@ -66,14 +68,36 @@ function Navbar() {
               Berita
             </Link>
           </li>
-          <li className="navbar-item">
-            <Link
-              to="/siadudu"
-              className="navbar-link"
-              onClick={closeMobileMenu}
-            >
-              Layanan Pengaduan
-            </Link>
+
+          <li
+            className="navbar-item"
+            onMouseEnter={() => setDropdown(true)}
+            onMouseLeave={() => setDropdown(false)}
+            onClick={toggleDropdown}
+          >
+            <span className="navbar-link dropdown-toggle">
+              Layanan <FaCaretDown className="caret-icon" />
+            </span>
+            <ul className={dropdown ? "dropdown-menu active" : "dropdown-menu"}>
+              <li>
+                <Link
+                  to="/akademik"
+                  className="dropdown-link"
+                  onClick={closeMobileMenu}
+                >
+                  Akademik
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/siadudu"
+                  className="dropdown-link"
+                  onClick={closeMobileMenu}
+                >
+                  SiAduDu (Pengaduan)
+                </Link>
+              </li>
+            </ul>
           </li>
 
           {/* AUTH LINKS */}
